6c7b7426b1df56de1c870adbdd77b690c8de9c2e
Techmad <techmad60@gmail.com>
2025-06-18 10:24:39 +0100

    fixed react suspense issue..

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,7 +1,13 @@
 // app/main/page.tsx
-
+import { Suspense } from "react";
 import MainClient from "@/components/MainPage";
+import CoinTableSkeleton from "@/components/SkeletonTable";
 
 export default function MainPage() {
-  return <MainClient />;
+  return (
+    <Suspense fallback={<div><CoinTableSkeleton/></div>}>
+      <MainClient />
+    </Suspense>
+  );
+  
 }
