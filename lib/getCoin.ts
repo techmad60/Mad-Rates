e9f9fecff0aacb1b4d02b7b9fc53c81e5628b1e4
Techmad <techmad60@gmail.com>
2025-06-21 13:45:37 +0100

    fixed revalidation

--- lib/getCoin.ts
+++ lib/getCoin.ts
@@ -2,7 +2,7 @@
 export async function getCoins(page: number = 1) {
   const res = await fetch(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=${page}&sparkline=false&price_change_percentage=1h,24h,7d`,
-    { next: { revalidate: 90000 } }
+    { next: { revalidate: 60 } }
   );
 
   if (!res.ok) throw new Error("Failed to fetch CoinGecko data");
